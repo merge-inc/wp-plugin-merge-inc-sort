28461a7780d212f7713ac3e7650f93839bac485e
Christos Athanasiadis <chris.k.athanasiadis@gmail.com>
2024-12-30 13:50:02 +0000

    chore: test automated commit

--- templates/generic-message-notice.php
+++ templates/generic-message-notice.php
@@ -1,3 +1,3 @@
 <div class="notice woocommerce-message woocommerce-admin-promo-messages is-dismissible notice-shadow">
-    <p>📊 | Sort</p>
+    <p>📊 | <strong>Sort</strong> <span id="ms-generic-message"></span></p>
 </div>
